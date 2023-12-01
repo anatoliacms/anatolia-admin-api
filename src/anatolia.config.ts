3ac1ed6a789e73fe8063fc47de6866d9f5b6fdc1
gunerkaanalkim <g.kaanalkim@gmail.com>
2023-12-01 16:52:44 +0000

    dynamic module implementation

---
+++ src/anatolia.config.ts
@@ -0,0 +1,12 @@
+export interface AnatoliaConfiguration {
+    database: {
+        type: string
+        host: string
+        port: number
+        username: string
+        password: string
+        database: string
+        autoLoadEntities: boolean
+        synchronize: boolean
+    }
+}
--- src/anatoliacms.module.ts
+++ src/anatoliacms.module.ts
@@ -1,14 +1,24 @@
 import {DynamicModule, Module} from '@nestjs/common';
 import {ContentTypeModule} from "./content-type/content-type.module";
 import {ProjectModule} from "./project/project.module";
+import {AnatoliaConfiguration} from "./anatolia.config";
 
 @Module({})
 export class AnatoliaCMSModule {
-    static register(): DynamicModule {
+    static register(anatoliaConfiguration: AnatoliaConfiguration): DynamicModule {
         return {
             module: AnatoliaCMSModule,
-            imports: [ProjectModule, ContentTypeModule],
+            providers: [
+                {
+                    provide: 'AnatoliaConfiguration',
+                    useValue: anatoliaConfiguration,
+                }
+            ],
+            imports: [
+                ProjectModule.register(anatoliaConfiguration),
+                ContentTypeModule.register(anatoliaConfiguration)
+            ],
             exports: [ProjectModule, ContentTypeModule]
         }
-    }Ï
+    }
 }
